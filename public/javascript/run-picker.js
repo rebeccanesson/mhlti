25baa8a80c36b3a8539af2fd24113fdb3b955fe0
James Brockman <james.brockman@gmail.com>
2014-07-18 13:44:05 -0400

    Edits to Javascript Labels

--- public/javascript/run-picker.js
+++ public/javascript/run-picker.js
@@ -1,4 +1,8 @@
 $(document).ready(
   function() {
-    $("select").imagepicker();
+	jQuery("select.image-picker.show-labels").imagepicker({
+	      hide_select:  true,
+	      show_label:   true,
+	    });
+
   });
