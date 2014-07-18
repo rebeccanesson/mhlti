736973d4f73fbc17675f9f7d74335e8f98d2bf36
douglashall <dhall@hbs.edu>
2014-07-18 15:43:20 -0400

    Adding console log to event handler

--- public/javascript/run-picker.js
+++ public/javascript/run-picker.js
@@ -1,10 +1,10 @@
 $(document).ready(
   function() {
 	jQuery("select.image-picker.show-labels").imagepicker({
-	      hide_select:  true,
-	      show_label:   true,
-	    }).on("change",function(){
-		
+	    hide_select:  true,
+	    show_label:   true,
+	}).change(function(evt){
+            console.log(evt); 
 	});
 		
   });
